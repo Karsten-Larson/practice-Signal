ef154154cac0fbcf6a9581f484c07ef76f7dd6b1
Karsten Larson <karsten.larson.1@gmail.com>
2025-03-31 11:52:07 -0500

    Day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,24 @@ import { DataServices } from './data.service';
   selector: 'app-root',
   imports: [RouterOutlet, InputComponent],
   template: `
-      <h1>Angular Services Demo</h1>
+    <h1>Angular Services Demo</h1>
     <div>
-    <app-input (output)="receiveInput($event)"></app-input> 
+      <app-input></app-input>
     </div>
-    @for(item of items; track item){
-      <div>{{item}}</div>
+    @for(item of items(); track item){
+    <div>{{ item }}</div>
     }
   `,
-  styles: ``
+  styles: ``,
 })
 export class AppComponent {
-  
   dataService: DataServices = inject(DataServices);
 
-  items: string[] = [];
-
-  ngOnInit() {
-    this.fetchItems();
-  }
-  //event handler function called when EventEmitter object 'output'
-  //emits a value through output binding
-  receiveInput(msg:string){
-    if(msg == "")
-      this.fetchItems();
-    this.items = this.items.filter(item => item.includes(msg))
-  }
-  //helper method to fetch items from DataService
-  fetchItems(){
-    this.items = this.dataService.getData();
-  }
+  items = computed(() =>
+    this.dataService
+      .getData()
+      .filter((item) =>
+        item.toLowerCase().includes(this.dataService.searchTerm().toLowerCase())
+      )
+  );
 }
